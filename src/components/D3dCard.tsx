a0efef4d69240cfd1fafe7180e12f5edb3d3fa12
alexander <alex43bg@gmail.com>
2024-03-04 18:33:07 +0200

    04.03_+

--- src/components/D3dCard.tsx
+++ src/components/D3dCard.tsx
@@ -2,13 +2,32 @@ import React from "react";
 
 const D3dCard = () => {
   return (
-    <div className="container max-w-[1700px] mx-auto">
-      <div className="wrapper mx-auto w-[400px] h-[400px] overflow-hidden">
-        <div className="imgbox absolute text-white">
-         hallo
+    <div className='w-full h-full'>
+      <div className="container border">
+      <div className="imgbox">
+        <img src="assets/images/.png" alt="" className="imgC"></img>
+      </div>
+      <span className="object objblue"></span>
+      <span className="bgtext">Bass</span>
+      <div className="title">
+        Lorem ipsum dolor
+      </div>
+      <div className="buySection">
+        <h2>
+          <span className='sp'>45.99</span> <sup className='sup'>$</sup>
+        </h2>
+        <div className='button'>
+        <a
+          className=""
+        >
+          Learn More!
+        </a>
         </div>
+        
       </div>
     </div>
+    </div>
+    
   );
 };
 
