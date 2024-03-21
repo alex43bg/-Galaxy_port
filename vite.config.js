1688befbe10dfde957815d0ef02c5a6d379f5bae
alexander <alex43bg@gmail.com>
2024-03-21 16:09:12 +0200

    21,03

--- vite.config.js
+++ vite.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
 	},
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	build: {
-		outDir: './dist',
+		outDir: './build_ready',
 		emptyOutDir: true,
 		sourcemap: false,
 		rollupOptions: {
